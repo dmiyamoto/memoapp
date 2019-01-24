2d7d142819c190ba0451784fe482f832e507b0ac
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-25 00:04:12 +0900

    Transform Memo.vue into typescript style.

--- components/Memo.vue
+++ components/Memo.vue
@@ -11,7 +11,7 @@
     >
       <div
         class="minus"
-        @click="$emit('minus')"
+        @click="minus"
       >
         X
       </div>
@@ -20,46 +20,47 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    top: {
-      type: Number,
-      default: 0
-    },
-    left: {
-      type: Number,
-      default: 0
-    },
-    index: {
-      type: Number,
-      default: 0
+<script lang="ts">
+import { Prop, Emit, Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Memo extends Vue {
+  // props
+  @Prop() top: number;
+  @Prop() left: number;
+  @Prop() index: number;
+
+  // data
+  public isDragging: boolean = false;
+
+  // emit
+  @Emit()
+  mouseMove(obj: { top: number, left: number, index: number }) {};
+
+  @Emit()
+  minus() {}
+
+  // methods
+  onMouseDown(e: any): void {
+    this.isDragging = true
+  }
+
+  onMouseMove(e: any): void {
+    if (this.isDragging) {
+      // topの-30とleftの-80はメモをdrag&dropで移動する際カーソルズレによって左側方向に移動できない事を回避するために付与
+      this.mouseMove({ top: e.pageY - 30, left: e.pageX - 80, index: this.index })
     }
-  },
-  data() {
-    return {
-      isDragging: false
-    }
-  },
-  methods: {
-    onMouseDown(e) {
-      this.isDragging = true
-    },
-    onMouseMove(e) {
-      if (this.isDragging) {
-        // topの-30とleftの-80はメモをdrag&dropで移動する際カーソルズレによって左側方向に移動できない事を回避するために付与
-        this.$emit('mouse-move', { top: e.pageY - 30, left: e.pageX - 80, index: this.index })
-      }
-    },
-    onMouseUp(e) {
-      this.isDragging = false
-    },
-    styles(top, left) {
-      return ({
-        top: `${top}px`,
-        left: `${left}px`
-      })
-    }
+  }
+
+  onMouseUp(e: any): void {
+    this.isDragging = false
+  }
+
+  styles(top: number, left: number) {
+    return ({
+      top: `${top}px`,
+      left: `${left}px`
+    })
   }
 }
 </script>
